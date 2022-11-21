d24613ccb80746a629b54c42c43dd4ed7da37791
ekta2608 <112929206+ekta2608@users.noreply.github.com>
2022-11-21 11:22:01 +0530

    Add files via upload

---
+++ whileloop.c
@@ -0,0 +1,28 @@
+/*while condtion
+
+
+syntax for while loop
+
+ initialization
+ 
+ while() condtion
+ {
+ printf("");
+ i++ upatation
+ 
+ } */
+ 
+ 
+ 
+ #include<stdio.h>
+ void main()
+ {
+ 	int i;
+ 	while(1);
+ 	{
+ 		printf("hello");
+ 		
+	 }
+ }
+
+
